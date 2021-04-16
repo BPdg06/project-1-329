ceab8cf14cc96250c2cc3f0a69d284d53c2d8386
Dong Geon Brian Park <brianpark@Dongui-MacBook-Pro.local>
2021-04-16 12:51:50 -0400

    updating2

--- js/app.js
+++ js/app.js
@@ -29,11 +29,17 @@ $.ajax("https://spreadsheets.google.com/feeds/list/1x7L4nA2fjjO3LgSizQn75avLHsrn
         console.log(project)
         let $projectCard = $(`
             <div>
-                <img src="${project.image}" />
-                <h1>${project.project}</h1>
-                <p>${project.description}</p>
+                <img src="${project.image}"/>
+                <h4>${project.project}</h4>
+                <p class="project_description">${project.description}</p>
+
+                <a href="${project.gitURL}" target="_blank">
+                    <img src="image/github.png" class="github_icon"></a>
+                <a href="${project.liveURL}" target="_blank">
+                    <img src="image/internet.png" class="internet_icon"></a>
             </div>
         `)
+
         $('div.project_content').append($projectCard)
     })
 
@@ -45,3 +51,17 @@ $.ajax("https://spreadsheets.google.com/feeds/list/1x7L4nA2fjjO3LgSizQn75avLHsrn
 .catch((error) => {
     console.error(error)
 })
+
+
+//javascript hamburger navigation bar
+window.addEventListener("scroll", function() {
+    const header = document.querySelector("header");
+});
+
+const hamburgerBar = document.querySelector(".hamburgerBar");
+const nav = document.querySelector(".navigationBar");
+
+hamburgerBar.addEventListener("click", () => {
+    hamburgerBar.classList.toggle("active");
+    nav.classList.toggle("active");
+});
